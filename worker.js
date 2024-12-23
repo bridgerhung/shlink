57b9756557e9ab37769cb7d49f9404e4aa029931
Bridger Hung <jnkjko123@gmail.com>
2024-12-23 22:20:57 +0800

    Refactor popover initialization in worker.js for improved functionality and maintainability

--- worker.js
+++ worker.js
@@ -186,11 +186,13 @@ function buildMaintenance(lang) {
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-    <script>
-      var popover = new bootstrap.Popover(document.querySelector('.example-popover'), {
-      container: 'body'
-        })
-    </script>
+  <!-- Replace the existing Popover initialization script -->
+  <script>
+  var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+  var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl)
+  })
+</script>
 </body>
 </html>`;
 }
